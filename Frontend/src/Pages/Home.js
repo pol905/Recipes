1c0d833859eac82e34b9f3187d265f029ec045c0
Siddhanth B N <56065640+pol905@users.noreply.github.com>
2020-12-28 22:23:33 +0530

    Added home page

---
+++ Frontend/src/Pages/Home.js
@@ -0,0 +1,263 @@
+import React, { useEffect, useState } from "react";
+import "../assets/css/styles.css";
+import about from "../assets/img/about.jpg";
+import home from "../assets/img/home.png";
+import ScrollReveal from "scrollreveal";
+import axios from "axios";
+
+const Home = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    async function getSession() {
+      console.log(await axios.get("/v1/whoami/"));
+    }
+    getSession();
+  }, []);
+  useEffect(() => {
+    const showMenu = (toggleId, navId) => {
+      const toggle = document.getElementById(toggleId),
+        nav = document.getElementById(navId);
+
+      // Validate that variables exist
+      if (toggle && nav) {
+        toggle.addEventListener("click", () => {
+          // We add the show-menu class to the div tag with the nav__menu class
+          nav.classList.toggle("show-menu");
+        });
+      }
+    };
+    showMenu("nav-toggle", "nav-menu");
+
+    /*==================== REMOVE MENU MOBILE ====================*/
+    const navLink = document.querySelectorAll(".nav__link");
+
+    function linkAction() {
+      const navMenu = document.getElementById("nav-menu");
+      // When we click on each nav__link, we remove the show-menu class
+      navMenu.classList.remove("show-menu");
+    }
+    navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+    /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+    const sections = document.querySelectorAll("section[id]");
+
+    function scrollActive() {
+      const scrollY = window.pageYOffset;
+
+      sections.forEach((current) => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 50;
+        let sectionId = current.getAttribute("id");
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+          document
+            .querySelector(".nav__menu a[href*=" + sectionId + "]")
+            .classList.add("active-link");
+        } else {
+          document
+            .querySelector(".nav__menu a[href*=" + sectionId + "]")
+            .classList.remove("active-link");
+        }
+      });
+    }
+    window.addEventListener("scroll", scrollActive);
+
+    /*==================== CHANGE BACKGROUND HEADER ====================*/
+    function scrollHeader() {
+      const nav = document.getElementById("header");
+      // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
+      if (this.scrollY >= 200) nav.classList.add("scroll-header");
+      else nav.classList.remove("scroll-header");
+    }
+    window.addEventListener("scroll", scrollHeader);
+
+    /*==================== SHOW SCROLL TOP ====================*/
+    function scrollTop() {
+      const scrollTop = document.getElementById("scroll-top");
+      // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+      if (this.scrollY >= 560) scrollTop.classList.add("show-scroll");
+      else scrollTop.classList.remove("show-scroll");
+    }
+    window.addEventListener("scroll", scrollTop);
+
+    /*==================== DARK LIGHT THEME ====================*/
+    const themeButton = document.getElementById("theme-button");
+    const darkTheme = "dark-theme";
+    const iconTheme = "bx-sun";
+
+    // Previously selected topic (if user selected)
+    const selectedTheme = localStorage.getItem("selected-theme");
+    const selectedIcon = localStorage.getItem("selected-icon");
+
+    // We obtain the current theme that the interface has by validating the dark-theme class
+    const getCurrentTheme = () =>
+      document.body.classList.contains(darkTheme) ? "dark" : "light";
+    const getCurrentIcon = () =>
+      themeButton.classList.contains(iconTheme) ? "bx-moon" : "bx-sun";
+
+    // We validate if the user previously chose a topic
+    if (selectedTheme) {
+      // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+      document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+        darkTheme
+      );
+      themeButton.classList[selectedIcon === "bx-moon" ? "add" : "remove"](
+        iconTheme
+      );
+    }
+
+    // Activate / deactivate the theme manually with the button
+    themeButton.addEventListener("click", () => {
+      // Add or remove the dark / icon theme
+      document.body.classList.toggle(darkTheme);
+      themeButton.classList.toggle(iconTheme);
+      // We save the theme and the current icon that the user chose
+      localStorage.setItem("selected-theme", getCurrentTheme());
+      localStorage.setItem("selected-icon", getCurrentIcon());
+    });
+
+    /*==================== SCROLL REVEAL ANIMATION ====================*/
+    const sr = ScrollReveal({
+      origin: "top",
+      distance: "30px",
+      duration: 2000,
+      reset: true,
+    });
+
+    sr.reveal(
+      `.home__data, .home__img,
+            .about__data, .about__img,
+            .services__content, .menu__content,
+            .app__data, .app__img,
+            .contact__data, .contact__button,
+            .footer__content`,
+      {
+        interval: 200,
+      }
+    );
+  }, []);
+
+  return (
+    <>
+      <a href="#" className="scrolltop" id="scroll-top">
+        <i className="bx bx-chevron-up scrolltop__icon"></i>
+      </a>
+
+      <header className="l-header" id="header">
+        <nav className="nav bd-container">
+          <a href="#" className="nav__logo">
+            Tasty
+          </a>
+
+          <div className="nav__menu" id="nav-menu">
+            <ul className="nav__list">
+              <li className="nav__item">
+                <a href="#home" className="nav__link active-link">
+                  Home
+                </a>
+              </li>
+              <li className="nav__item">
+                <a href="#about" className="nav__link">
+                  About
+                </a>
+              </li>
+              <li className="nav__item">
+                <a href="#contact" className="nav__link">
+                  Contact us
+                </a>
+              </li>
+              <li className="nav__item">
+                <a href="" className="nav__link">
+                  {isLoggedIn ? "Logout" : "Login"}
+                </a>
+              </li>
+
+              <li>
+                <i className="bx bx-moon change-theme" id="theme-button"></i>
+              </li>
+            </ul>
+          </div>
+
+          <div className="nav__toggle" id="nav-toggle">
+            <i className="bx bx-menu"></i>
+          </div>
+        </nav>
+      </header>
+
+      <main className="l-main">
+        <section className="home" id="home">
+          <div className="home__container bd-container bd-grid">
+            <div className="home__data">
+              <h1 className="home__title">Tasty</h1>
+              <h2 className="home__subtitle">
+                Your secret recipe is <br /> safe with us.
+              </h2>
+              <a href="#" className="button">
+                Get started
+              </a>
+            </div>
+
+            <img src={home} alt="" className="home__img" />
+          </div>
+        </section>
+
+        <section className="about section bd-container" id="about">
+          <div className="about__container  bd-grid">
+            <div className="about__data">
+              <span className="section-subtitle about__initial">About us</span>
+              <h2 className="section-title about__initial">
+                We keep
+                <br /> your secret
+              </h2>
+              <p className="about__description">
+                Your special food's recipe needs a place to be kept safe and be
+                protected. Using tasty you can categorize all your recipes safe
+                and organised in one place.
+              </p>
+              <a href="#" className="button">
+                Get started
+              </a>
+            </div>
+
+            <img src={about} alt="" className="about__img" />
+          </div>
+        </section>
+        <section className="contact section bd-container" id="contact">
+          <div className="contact__container bd-grid">
+            <div className="contact__data">
+              <span className="section-subtitle contact__initial">
+                Let's talk
+              </span>
+              <h2 className="section-title contact__initial">Contact us</h2>
+              <p className="contact__description">
+                If you want to give suggestions and feedbacks please do let us
+                know on tasty@gmail.com.
+              </p>
+            </div>
+            <div className="contact__button">
+              <a href="#" className="button">
+                Contact us now
+              </a>
+            </div>
+          </div>
+        </section>
+      </main>
+      <footer>
+        <div className="footer__content">
+          <h3 className="footer__title">Adress</h3>
+          <ul>
+            <li>RNSIT </li>
+            <li>Karnataka </li>
+            <li>BANGALORE</li>
+            <li>tasty@gmail.com</li>
+          </ul>
+        </div>
+        <p className="footer__copy">
+          &#169; 2020 SID_SUP_CODE. All right reserved
+        </p>
+      </footer>
+    </>
+  );
+};
+
+export default Home;
